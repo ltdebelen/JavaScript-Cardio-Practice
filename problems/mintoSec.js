7d996564611fd97d6034bdb3827b767013775620
Lawrence Adrian De Belen <ltdebelen@gmail.com>
2020-05-28 21:07:21 -0500

    adding solveed for min to sec

---
+++ problems/mintoSec.js
@@ -0,0 +1,11 @@
+// Write a function that takes an integer minutes and converts it to seconds.
+
+// Ex: convert(5) -> 300;
+
+function convert(min) {
+  let result = min * 60;
+
+  return result;
+}
+
+console.log(convert(5));
