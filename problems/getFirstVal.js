dfe98f5195406b9c7b6be5b6d4c20a42854f74b8
Lawrence Adrian De Belen <ltdebelen@gmail.com>
2020-05-29 23:03:53 -0500

    Adding code for getting first val of array

---
+++ problems/getFirstVal.js
@@ -0,0 +1,9 @@
+// Create a function that takes an array and returns the first element.
+
+// Ex: getFirstVal([1, 2, 3]) returns 1
+
+function getFirstVal(arr) {
+  return arr[0];
+}
+
+console.log(getFirstVal([1, 2, 3]));
