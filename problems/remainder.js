903543e3cf27859ce7df514124503d9ec7c5c6b0
Lawrence Adrian De Belen <ltdebelen@gmail.com>
2020-06-21 14:26:39 -0500

    Adding solution for remainder

---
+++ problems/remainder.js
@@ -0,0 +1,9 @@
+// Return the remainder of the two numbers passed as parameters.
+
+// Ex: remainder(1,3) returns 1
+
+function remainder(num1, num2) {
+  return num1 % num2;
+}
+
+console.log(remainder(1, 3));
