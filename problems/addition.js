036d17d1b3228e0ed9608aa606f077d94a54174c
Lawrence Adrian De Belen <ltdebelen@gmail.com>
2020-06-24 17:50:12 -0500

    Adding solution for addition problem.

---
+++ problems/addition.js
@@ -0,0 +1,9 @@
+// Create a function that takes a number as an argument, increments the number by +1 and returns the result.
+
+// Ex: addition(0) ➞ 1
+
+function addition(num) {
+  return num + 1;
+}
+
+console.log(addition(0));
