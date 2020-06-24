de81a98226cc116d6735cacee092d979e72a931d
Lawrence Adrian De Belen <ltdebelen@gmail.com>
2020-06-23 19:41:35 -0500

    Adding solution for triArea

---
+++ problems/triArea.js
@@ -0,0 +1,9 @@
+// Write a function that takes the base and height of a triangle and return its area.
+
+// Ex: triArea(3, 2) ➞ 3
+
+function triArea(base, height) {
+  return (base * height) / 2;
+}
+
+console.log(triArea(3, 2));
