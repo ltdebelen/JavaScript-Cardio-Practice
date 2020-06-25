b0a2b1817fd88d3df9e1c92352bfcc9a496c53d4
Lawrence Adrian De Belen <ltdebelen@gmail.com>
2020-06-25 18:22:26 -0500

    Adding solution for getFirst Value

---
+++ problems/firstElement.js
@@ -0,0 +1,9 @@
+// Create a function that takes an array and returns the first element.
+
+// Ex: getFirstValue([1, 2, 3]) ➞ 1
+
+function getFirstValue(arr) {
+  return arr[0];
+}
+
+console.log(getFirstValue([1, 2, 3]));
