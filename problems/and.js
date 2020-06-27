0581457a84fba3abafff84f5c9c48a710ae807c3
Lawrence Adrian De Belen <ltdebelen@gmail.com>
2020-06-27 07:24:30 -0500

    Adding solution for AND problem

---
+++ problems/and.js
@@ -0,0 +1,10 @@
+// Make a function that evaluates and returns the value of an AND logical operator based on the parameters passed.
+
+// Ex: and(true, false) ➞ false
+// and(true, true) ➞ true
+
+function and(first, sec) {
+  return first && sec;
+}
+
+console.log(and(true, true));
