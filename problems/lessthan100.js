10ae3ca0aa8cc4efff663e4ec185ca61f2eccd85
Lawrence Adrian De Belen <ltdebelen@gmail.com>
2020-06-26 21:31:14 -0500

    Adding solution for lessthatn100

---
+++ problems/lessthan100.js
@@ -0,0 +1,10 @@
+// Given two numbers, return true if the sum of both numbers is less than 100. Otherwise return false.
+
+// lessThan100(22, 15) ➞ true
+// 22 + 15 = 37
+
+function lessThan100(first, second) {
+  return first + second < 100;
+}
+
+console.log(lessThan100(22, 15));
