e39a9180eff6c64cb2457df1ff3a9799ab798bb1
Lawrence Adrian De Belen <ltdebelen@gmail.com>
2020-07-05 19:55:26 -0500

    Adding solution for perimiter

---
+++ problems/findPerimiter.js
@@ -0,0 +1,9 @@
+// Create a function that takes height and width and finds the perimeter of a rectangle.
+
+// Ex: findPerimeter(6, 7) ➞ 26
+
+function findParameter(h, w) {
+  return h * 2 + w * 2;
+}
+
+console.log(findParameter(6, 7));
