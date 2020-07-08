0e6f4b52c8d72fbcc9520b31acd541e36ccee570
Lawrence Adrian De Belen <ltdebelen@gmail.com>
2020-07-07 20:56:02 -0500

    adding solution for isSameNum

---
+++ problems/isSameNum.js
@@ -0,0 +1,9 @@
+// Create a function that returns true when num1 is equal to num2; otherwise return false.
+
+// Ex: isSameNum(4, 8) ➞ false; isSameNum(2, 2) ➞  true
+
+function isSameNum(first, second) {
+  return first === second;
+}
+
+console.log(isSameNum(2, 2));
