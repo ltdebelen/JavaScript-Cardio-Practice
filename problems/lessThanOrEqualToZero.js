23df1e3654cd2c0000d86388b6f389cc29ecafdb
Lawrence Adrian De Belen <ltdebelen@gmail.com>
2020-07-08 19:50:35 -0500

    Adding solution for lessthan or equal to zero

---
+++ problems/lessThanOrEqualToZero.js
@@ -0,0 +1,9 @@
+// Create a function that takes a number as its only argument and returns true if it's less than or equal to zero, otherwise return false.
+
+// Ex: lessThanOrEqualToZero(5) ➞ false; lessThanOrEqualToZero(0) ➞ true
+
+function lessThanOrEqualToZero(num) {
+  return num <= 0;
+}
+
+console.log(lessThanOrEqualToZero(0));
