6134ffd80fd99703698044d81b8c075fc2bfea53
Lawrence Adrian De Belen <ltdebelen@gmail.com>
2020-07-11 08:04:35 -0500

    Adding solution for isempty

---
+++ problems/isEmpty.js
@@ -0,0 +1,9 @@
+// Create a function that returns true if a string is empty and false otherwise.
+
+// Ex: isEmpty("") ➞ true; isEmpty(" ") ➞ false
+
+function isEmpty(str) {
+  return str.length <= 0;
+}
+
+console.log(isEmpty(""));
