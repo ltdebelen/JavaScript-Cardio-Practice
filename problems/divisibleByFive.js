82f12e479b1d9064bdb7d762609610ee5234a8c1
Lawrence Adrian De Belen <ltdebelen@gmail.com>
2020-07-12 15:11:41 -0500

    Adding solution for divisible by 5

---
+++ problems/divisibleByFive.js
@@ -0,0 +1,9 @@
+// Create a function that returns true if an integer is evenly divisible by 5, and false otherwise.
+
+// Ex: divisibleByFive(5) ➞ true; divisibleByFive(37) ➞ false
+
+function divisibleByFive(num) {
+  return num % 5 === 0;
+}
+
+console.log(divisibleByFive(5));
