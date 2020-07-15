c3696836dd68f3a38a1c5284bd594a9e4667330b
Lawrence Adrian De Belen <ltdebelen@gmail.com>
2020-07-14 20:24:44 -0500

    adding solution for strLength

---
+++ problems/strLength.js
@@ -0,0 +1,9 @@
+// Write a function that returns the length of a string. Make your function recursive.
+
+// Ex: strLength("apple") ➞ 5; strLength("make") ➞ 4
+
+function strLength(str) {
+  return str.length;
+}
+
+console.log(strLength("apple"));
