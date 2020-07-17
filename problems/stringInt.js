2574cfa4a9077783a00ed3054b702c727fce9a75
Lawrence Adrian De Belen <ltdebelen@gmail.com>
2020-07-16 20:21:27 -0500

    Adding solution for string int

---
+++ problems/stringInt.js
@@ -0,0 +1,9 @@
+// Create a function that takes a string and returns it as an integer.
+
+// Ex: stringInt("6") ➞ 6; stringInt("1000") ➞ 1000
+
+function stringInt(strNum) {
+  return parseInt(strNum);
+}
+
+console.log(stringInt(6));
