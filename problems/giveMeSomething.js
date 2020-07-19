67d36511cbe45de6b55451957423dd81358557df
Lawrence Adrian De Belen <ltdebelen@gmail.com>
2020-07-19 18:57:02 -0500

    Adding solution for giveMeSomething

---
+++ problems/giveMeSomething.js
@@ -0,0 +1,9 @@
+// Write a function giveMeSomething that returns the word "something" joined with a space and the parameter a
+
+// giveMeSomething("is better than nothing") ➞ "something is better than nothing"; giveMeSomething("Bob Jane") ➞ "something Bob Jane"
+
+function giveMeSomething(str) {
+  return "something " + str;
+}
+
+console.log(giveMeSomething("is better than nothing"));
