8abb877e5b65392e47dc4f6f7a847da3e19fe7bb
Lawrence Adrian De Belen <ltdebelen@gmail.com>
2020-07-21 22:13:12 -0500

    Adding solution for concatName

---
+++ problems/concatName.js
@@ -0,0 +1,9 @@
+// Given two strings, firstName and lastName, return a single string in the format "last, first".
+
+// Ex: concatName("First", "Last") ➞ "Last, First"
+
+function concatName(first, last) {
+  return first + ", " + last;
+}
+
+console.log(concatName("First", "Last"));
